a9e49327464408f8d95c83f48959aed21998b11f
thonathan <januszewskieth@urbandaleschools.com>
2020-09-08 15:43:40 -0500

    jedi training final

--- 1.0_Jedi_Training.py
+++ 1.0_Jedi_Training.py
@@ -3,19 +3,19 @@ Sign your name:_ETHAN JANUSZEWSKI_______________
 
 
 
-Define Forking: 
-
-Define Cloning: 
-
+Define Forking:
+when someone makes a view only copy of a repository
+Define Cloning:
+making the repository that you copied editable
 Define Branching:
-
-Define Committing: 
+creating seperate lines of code that doesn't affect of master code
+Define Committing:
 a checkpoint in the development of a project
-Define Merging: 
-
+Define Merging:
+merging a bunch of code into one document
 Define Pushing:
-
+sending a file from pycharm to github
 Define Pull Request:
-
+sending your code to the person in charge
 
 '''
