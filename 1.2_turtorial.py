7bb2b2ba512e2e04533b31c749e6df2630ea4a84
thonathan <januszewskieth@urbandaleschools.com>
2020-09-08 15:50:01 -0500

    name,color

--- 1.2_turtorial.py
+++ 1.2_turtorial.py
@@ -8,28 +8,28 @@ Turtle Documentation: https://docs.python.org/3.3/library/turtle.html?highlight=
 
 '''
 import turtle
-yoda=turtle.Turtle()
+merc=turtle.Turtle()
 screen=turtle.Screen() # makes a screen object
 screen.bgcolor('black') # colors the screen
-yoda.pensize(3) # width of pen line
-yoda.speed(10)  # speed of drawing. Go fast to not waste time.
-yoda.color("#00FF00")
-yoda.circle(100)  #head
-yoda.penup()
-yoda.setpos(50,185) #right ear
-yoda.pendown()
-yoda.goto(200,210)
-yoda.goto(88,145)
-yoda.penup()
-yoda.setpos(-50,185)  #left ear
-yoda.pendown()
-yoda.goto(-200,210)
-yoda.goto(-88,145)
-yoda.penup()
-yoda.setpos(200,-300)
-yoda.pendown()
-yoda.pencolor('#00FF00')
+merc.pensize(3) # width of pen line
+merc.speed(10)  # speed of drawing. Go fast to not waste time.
+merc.color("#FF4500")
+merc.circle(100)  #head
+merc.penup()
+merc.setpos(50,185) #right ear
+merc.pendown()
+merc.goto(200,210)
+merc.goto(88,145)
+merc.penup()
+merc.setpos(-50,185)  #left ear
+merc.pendown()
+merc.goto(-200,210)
+merc.goto(-88,145)
+merc.penup()
+merc.setpos(200,-300)
+merc.pendown()
+merc.pencolor('#FF4500')
 
 
-yoda.write('Sign Your Name Here',font=("Arial", 16, "normal")) # signs your name to your art
+merc.write('Ethan Januszewski',font=("Arial", 16, "normal")) # signs your name to your art
 turtle.exitonclick() #Keeps pycharm window open so we can see the drawing
